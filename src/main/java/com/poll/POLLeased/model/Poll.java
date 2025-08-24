cf3f7c8126ee67d15a022095a96f7c23e976afa5
Gabe <gabepineda6@gmail.com>
2025-08-23 21:11:55 -0700

    made options list take in Vote obj rather than string and embed Vote into options

--- src/main/java/com/poll/POLLeased/model/Poll.java
+++ src/main/java/com/poll/POLLeased/model/Poll.java
@@ -1,15 +1,10 @@
 package com.poll.POLLeased.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Poll {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,18 +14,17 @@ public class Poll {
     //jpa creates separate table in db that takes the poll id and this options list as columns and data
     //using id as a foreign key pointing ot poll id and do this to have them related without making a new model
     @ElementCollection
-    private List<String> options = new ArrayList<>();
+    private List<Vote> options = new ArrayList<>();
 
-    @ElementCollection
-    private List<String> votes = new ArrayList<>();
+//    @ElementCollection
+//    private List<Vote> votes = new ArrayList<>();
 
     public Poll() {
     }
 
-    public Poll(String question, List<String> options, List<String> votes) {
+    public Poll(String question, List<Vote> options) {
         this.question = question;
         this.options = options;
-        this.votes = votes;
     }
 
     public Long getId() {
@@ -49,19 +43,11 @@ public class Poll {
         this.question = question;
     }
 
-    public List<String> getOptions() {
+    public List<Vote> getOptions() {
         return options;
     }
 
-    public void setOptions(List<String> options) {
+    public void setOptions(List<Vote> options) {
         this.options = options;
-    }
-
-    public List<String> getVotes() {
-        return votes;
-    }
-
-    public void setVotes(List<String> votes) {
-        this.votes = votes;
     }
 }
