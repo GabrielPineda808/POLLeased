11e132db0b96db97368eb1d2706e6c95f4b4e7e0
Gabe <gabepineda6@gmail.com>
2025-08-23 18:13:54 -0700

    Created JPA repository and a get all polls method to be implemented

--- src/main/java/com/poll/POLLeased/repository/PollRepository.java
+++ src/main/java/com/poll/POLLeased/repository/PollRepository.java
@@ -1,4 +1,13 @@
 package com.poll.POLLeased.repository;
 
-public class PollRepository {
+import com.poll.POLLeased.model.Poll;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PollRepository extends JpaRepository<Poll, Long> {
+    List<Poll> getAllPoll();
+    Poll getPollById(Long id);
 }
