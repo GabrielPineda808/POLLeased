81880621b9f42a30bbc2430ab4966e2a48ba448a
Gabe <gabepineda6@gmail.com>
2025-08-23 18:14:55 -0700

    Created service layer to interact with db repo

--- src/main/java/com/poll/POLLeased/service/PollService.java
+++ src/main/java/com/poll/POLLeased/service/PollService.java
@@ -1,4 +1,16 @@
 package com.poll.POLLeased.service;
 
+import com.poll.POLLeased.model.Poll;
+import com.poll.POLLeased.repository.PollRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PollService {
+    private PollRepository pr;
+
+    public List<Poll> getAllPolls(){
+        return pr.getAllPoll();
+    }
 }
