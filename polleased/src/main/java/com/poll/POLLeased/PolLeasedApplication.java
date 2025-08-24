0e7348931f4b55f39e85736c14a0fc8c4a63914d
Gabe <gabepineda6@gmail.com>
2025-08-23 22:42:34 -0700

    added backend and front end folders

---
+++ polleased/src/main/java/com/poll/POLLeased/PolLeasedApplication.java
@@ -0,0 +1,13 @@
+package com.poll.POLLeased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolLeasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolLeasedApplication.class, args);
+	}
+
+}
---
+++ polleased/src/test/java/com/poll/POLLeased/PolLeasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poll.POLLeased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolLeasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
