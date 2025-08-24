fc477fb51444bf65840f644d834028f7e4d6953d
Gabe <gabepineda6@gmail.com>
2025-08-23 22:51:57 -0700

    Revert "removed files"
    This reverts commit c801e85a88c1edd05ba0d7f72307d4732d783e1b.

---
+++ src/main/java/com/poll/POLLeased/controller/PollController.java
@@ -0,0 +1,40 @@
+package com.poll.POLLeased.controller;
+
+import com.poll.POLLeased.dto.Vote;
+import com.poll.POLLeased.model.Poll;
+import com.poll.POLLeased.model.OptionVote;
+import com.poll.POLLeased.service.PollService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/poll")
+public class PollController {
+    private PollService ps;
+
+    public PollController(PollService ps) {
+        this.ps = ps;
+    }
+
+    @PostMapping
+    public Poll createPoll(@RequestBody Poll poll){
+        return ps.save(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAll(){
+        return ps.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPoll(@PathVariable long id){
+        return ps.getPoll(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote){
+        ps.vote(vote.getPollId(), vote.getOptionIndex());
+    }
+}
